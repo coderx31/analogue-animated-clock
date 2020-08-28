d0e6c8b8abefc178036fa279c48ed89b37308c62
coderx3134 <shenal.fernando10@gmail.com>
2020-08-28 19:18:41 +0530

    create the animatedClock java class

---
+++ src/AnimatedClock.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AnimatedClock extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
